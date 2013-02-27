624ac829d098067a1857c67d8a5e9127cb71a265
abhishek srivastava <abhishek77in@gmail.com>
2013-02-28 01:21:22 +0530

    minor update

--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -1,11 +1,11 @@
 module RepositoriesHelper
 
   def title(repo)
-    link_to repo.name, show_repo_path(repo.owner, repo.name)
+    link_to repo.name.capitalize, show_repo_path(repo.owner, repo.name)
   end
 
   def description(repo)
-    "Watchers: <b>#{repo.watchers}</b><br/>#{repo.description}<br/>- created by: <b>#{repo.owner}</b>".html_safe
+    "Watchers: <b>#{repo.watchers}</b><br/>#{repo.description}<br/>Created by: <b>#{repo.owner}</b>".html_safe
   end
 
 end
