ad76f09c2394f57e4df8eeabfa0f7fd47412affb
abhishek srivastava <abhishek77in@gmail.com>
2013-03-02 11:43:41 +0530

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User
+  include Mongoid::Document
+  field :provider, type: String
+  field :uid, type: String
+  field :name, type: String
+  field :email, type: String
+  attr_accessible :provider, :uid, :name, :email
+  # run 'rake db:mongoid:create_indexes' to create indexes
+  index({ email: 1 }, { unique: true, background: true })
+
+  def self.from_omniauth(auth)
+    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+  end
+
+  def self.create_from_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      if auth['info']
+         user.name = auth['info']['name'] || ""
+         user.email = auth['info']['email'] || ""
+      end
+    end
+  end
+
+end
