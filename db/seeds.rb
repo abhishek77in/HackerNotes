8be1af731037d659f6e96a9416999ffc895c3b67
abhishek srivastava <abhishek77in@gmail.com>
2013-03-04 00:36:04 +0530

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,23 @@
-def new_blog_attributes
+def title_excerpt_url
   { title: Faker::Lorem.words(8).join(" "),
     excerpt: Faker::Lorem.paragraph,
     url: Faker::Internet.url }
 end
 
+def subs_service_attributes
+  { title: Faker::Lorem.words(8).join(" "),
+    description: Faker::Lorem.paragraph,
+    url: Faker::Internet.url }
+end
+
 Repository.all.each do |r|
   10.times do
-    r.blogs.create(new_blog_attributes)
+    r.subscription_services.create(subs_service_attributes)
+    r.blogs.create(title_excerpt_url)
+    r.tips.create(title_excerpt_url)
+    r.slides.create(title_excerpt_url)
+    r.qnas.create(title_excerpt_url)
+    r.screencasts.create(title_excerpt_url)
+    r.noteworthies.create(title_excerpt_url)
   end
 end
