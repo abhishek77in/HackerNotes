eee84fa1a0bbca807b1c0bf7ab3d284c3c6d02c8
abhishek srivastava <abhishek77in@gmail.com>
2013-03-03 20:13:00 +0530

    add notes to repo#show

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,6 +6,13 @@ class RepositoriesController < ApplicationController
 
   def show_repo
     @repo = GithubService.new.fetch(params[:owner], params[:name])
+    @subs_services = @repo.subscription_services.all
+    @blogs = @repo.blogs.all
+    @tips = @repo.tips.all
+    @slides = @repo.slides.all
+    @qnas = @repo.qnas.all
+    @noteworthies = @repo.noteworthies.all
+    @screencasts = @repo.screencasts.all
   end
 
 end
