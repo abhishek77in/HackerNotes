8b695ce363d96837f8666315ec49a9d5f7c4421a
abhishek srivastava <abhishek77in@gmail.com>
2013-03-15 00:51:33 +0530

    add jimmy as module

---
+++ app/models/jimmy.rb
@@ -0,0 +1,50 @@
+module Jimmy
+  def create_videos
+    keyword = self.name.to_a + self.description.split(' ').take(8)
+    keyword = keyword.join(' ')
+    resources = GoogleVideo.fetch(keyword)
+    resources.each do |resource|
+      self.videos.create(resource.attributes)
+    end
+  end
+
+  def create_qnas
+    resources = Stackoverflow.fetch(self.name)
+    resources.each do |resource|
+      self.qnas.create(resource.attributes)
+    end
+  end
+
+  def create_noteworthies
+    keyword = self.name.to_a + self.description.split(' ').take(8)
+    keyword = keyword.join(' ')
+    resources = GoogleWeb.fetch(keyword)
+    resources.each do |resource|
+      self.noteworthies.create(resource.attributes)
+    end
+  end
+
+  def create_slides
+    keyword = self.name.gsub(/_|-/,' ')
+    slides = SpeakerDeck.fetch(keyword)
+    slides.each do |slide|
+      self.slides.create(slide.attributes)
+    end
+  end
+
+  def create_blogs
+    keyword = self.name.to_a + self.description.split(' ').take(8)
+    keyword = keyword.join(' ')
+    blogs = GoogleBlog.fetch(keyword)
+    blogs.each do |blog|
+      self.blogs.create(blog.attributes)
+    end
+  end
+
+  def create_tweeters
+    keyword = self.name.gsub(/_|-/,' ')
+    TweeterService.fetch(keyword).each do |t|
+      self.tweeters.create(t.attributes)
+    end
+  end
+end
--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,5 +1,6 @@
 class Repository
   include Mongoid::Document
+  include Jimmy
   field :name, type: String
   field :owner, type: String
   field :full_name, type: String
@@ -10,61 +11,12 @@ class Repository
   after_create :create_from_service
 
   def create_from_service
-    create_blogs_from_service
-    create_slides_from_service
-    create_tweeters_from_service
-    create_noteworthies_from_service
-    create_qnas_from_service
-    create_videos_from_service
-  end
-
-  def create_videos_from_service
-    keyword = self.name.to_a + self.description.split(' ').take(8)
-    keyword = keyword.join(' ')
-    resources = GoogleVideo.fetch(keyword)
-    resources.each do |resource|
-      self.videos.create(resource.attributes)
-    end
-  end
-
-  def create_qnas_from_service
-    resources = Stackoverflow.fetch(self.name)
-    resources.each do |resource|
-      self.qnas.create(resource.attributes)
-    end
-  end
-
-  def create_noteworthies_from_service
-    keyword = self.name.to_a + self.description.split(' ').take(8)
-    keyword = keyword.join(' ')
-    resources = GoogleWeb.fetch(keyword)
-    resources.each do |resource|
-      self.noteworthies.create(resource.attributes)
-    end
-  end
-
-  def create_slides_from_service
-    keyword = self.name.gsub(/_|-/,' ')
-    slides = SpeakerDeck.fetch(keyword)
-    slides.each do |slide|
-      self.slides.create(slide.attributes)
-    end
-  end
-
-  def create_blogs_from_service
-    keyword = self.name.to_a + self.description.split(' ').take(8)
-    keyword = keyword.join(' ')
-    blogs = GoogleBlog.fetch(keyword)
-    blogs.each do |blog|
-      self.blogs.create(blog.attributes)
-    end
-  end
-
-  def create_tweeters_from_service
-    keyword = self.name.gsub(/_|-/,' ')
-    TweeterService.fetch(keyword).each do |t|
-      self.tweeters.create(t.attributes)
-    end
+    create_blogs
+    create_slides
+    create_tweeters
+    create_noteworthies
+    create_qnas
+    create_videos
   end
 
   embeds_many :videos
