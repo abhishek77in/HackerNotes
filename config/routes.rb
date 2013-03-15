6100d1bc2b2107ef9aaa4df892975d1d910fbdf0
abhishek srivastava <abhishek77in@gmail.com>
2013-03-15 23:45:14 +0530

    remove commented out routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,57 +3,9 @@ Hackernotes::Application.routes.draw do
   root :to => 'home#homepage'
   get 'search', :to => 'repositories#search'
 
-  get ':owner/:name', :to => 'repositories#show_repo', :as => 'show_repo'
-
   match '/auth/github/callback' => 'sessions#create', :as => :signin
   match '/signout' => 'sessions#destroy', :as => :signout
   match 'auth/failure', to: redirect('/')
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
+  get ':owner/:name', :to => 'repositories#show_repo', :as => 'show_repo'
 end
