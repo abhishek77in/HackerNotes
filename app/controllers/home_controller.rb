ac3bc134156b6029e1f3e45280563e46ceb75ffc
abhishek srivastava <abhishek77in@gmail.com>
2013-04-06 21:12:23 +0530

    show popular_repos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def homepage
-    @repos = Repository.limit(30)
+    @repos = Repository.desc(:created_at).limit(10)
+    @popular_repos = Repository.desc(:watchers).limit(10)
   end
 end
