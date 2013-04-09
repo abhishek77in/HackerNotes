fb05685ae058ef92de90ce190eaae6282fdbb33c
abhishek srivastava <abhishek77in@gmail.com>
2013-04-09 23:22:56 +0530

    Fix: Show only relevant titles in sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
 
+  def sidebar(resources, title, css_class = nil, anchor = nil)
+    return nil if resources.empty?
+    anchor = anchor || resources.name.downcase.pluralize
+    { anchor: "##{anchor}", title: title, css_class: css_class }
+  end
+
   def global_validation_message
     if flash[:alert].present?
       %Q[<div class="globalValidationMessage">#{flash[:alert]}</div>].html_safe
