d6c1afea773ee0798256c037f8c1c63e3956c649
abhishek srivastava <abhishek77in@gmail.com>
2013-04-14 23:08:34 +0530

    Add title helper

--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -1,7 +1,20 @@
 module RepositoriesHelper
 
+ NOTES = { 'Blogs' => 'blogs',
+            'Subscribe' => 'subscribe',
+            "Tips n Tricks" => 'tips',
+            'Slides' => 'slides',
+            'FAQ' => 'qnas',
+            'Screencasts' => 'screencasts',
+            'Videos' => 'videos',
+            'Noteworthy' => 'noteworthies' }
+
   def title_link(repo)
     link_to repo.name.capitalize, show_repo_path(repo.owner, repo.name)
+  end
+
+  def title(resources)
+    NOTES.invert[resources.name.to_s.downcase.pluralize]
   end
 
   def description(repo)
