26069efb3676a366aba44e998be5c7e7a7338fb5
abhishek srivastava <abhishek77in@gmail.com>
2013-04-19 01:31:28 +0530

    Improve CSS

--- app/assets/javascripts/repositories.js
+++ app/assets/javascripts/repositories.js
@@ -1,16 +1,11 @@
 $(document).ready(function() {
-  $(".fancySelectBox").selectBox();
-
+  // $(".fancySelectBox").selectBox();
   slider();
 });
 
 function slider () {
   $('nav ul li a').click(function() {
-    console.log('1');
-    console.log(this);
     $('nav ul li').removeClass('active');
-    console.log('2');
     $(this).parent().addClass('active');
-    console.log('3');
   });
 }
