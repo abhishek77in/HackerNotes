8030ee6697fe482bd5df593a7a17377489c851be
abhishek srivastava <abhishek77in@gmail.com>
2013-04-21 21:52:40 +0530

    Remove delete method (need not be implemented)

--- app/controllers/notes_controller.rb
+++
@@ -1,9 +0,0 @@
-class NotesController < ApplicationController
-
-  def delete
-    repo = GithubService.new.fetch(params[:owner], params[:name])
-    resource = repo.send(params[:resource]).find(id: params[:id])
-    return unless current_user.nickname == repo.owner
-    resource.destroy
-  end
-end
