905add67e6c4abb22a92d2504d85359b4a1b6f3c
abhishek srivastava <abhishek77in@gmail.com>
2013-04-25 00:37:26 +0530

    WIP whitelist slides

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,3 +1,13 @@
 class Slide
   include Plug
+  HOSTNAME_WHITELIST = %w(www.speakerdeck.com speakerdeck.com  www.slideshare.net slideshare.net)
+  validate :whitelisted?, on: :create
+
+  private
+  def whitelisted?
+    hostname = URI.parse(self.url).hostname
+    unless HOSTNAME_WHITELIST.include?(hostname)
+      errors.add(:whitelist, "resource should belong to #{HOSTNAME_WHITELIST.join(', ')}")
+    end
+  end
 end
