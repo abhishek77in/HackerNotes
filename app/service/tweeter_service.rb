1bf7a00d543ebdb5682afd08f0a56f2da60b544d
abhishek srivastava <abhishek77in@gmail.com>
2013-04-25 22:59:09 +0530

    Update image to thumbnail_url

--- app/service/tweeter_service.rb
+++ app/service/tweeter_service.rb
@@ -1,5 +1,5 @@
 class TweeterService
-  attr_accessor :title, :url, :description, :image
+  attr_accessor :title, :url, :description, :thumbnail_url
 
   REQ_URL = "https://twitter.com/search/users?q="
   BASE_URL = "https://twitter.com"
@@ -14,14 +14,14 @@ class TweeterService
     @title = tweeter.search(TITLE).text
     @url = BASE_URL + tweeter.search(URL).first["href"]
     @description = tweeter.search(DESCRIPTION).text
-    @image = tweeter.search(IMAGE).first["src"]
+    @thumbnail_url = tweeter.search(IMAGE).first["src"]
   end
 
   def attributes
     { title: title,
       url: url,
       description: description,
-      image: image }
+      thumbnail_url: thumbnail_url }
   end
 
   def self.fetch(keyword)
