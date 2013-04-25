dd74e6879b1bfa66881962ffbd634c62b24b2d42
abhishek srivastava <abhishek77in@gmail.com>
2013-04-26 00:53:39 +0530

    Whiltelist websites for qna

--- app/models/qna.rb
+++ app/models/qna.rb
@@ -1,3 +1,13 @@
 class Qna
   include Plug
+  HOSTNAME_WHITELIST = %w(www.stackoverflow.com stackoverflow.com www.quora.com quora.com)
+  validate :whitelisted?, on: :create
+
+  private
+  def whitelisted?
+    hostname = URI.parse(self.url).hostname
+    unless HOSTNAME_WHITELIST.include?(hostname)
+      errors.add(:whitelist, "resource should belong to #{HOSTNAME_WHITELIST.join(', ')}")
+    end
+  end
 end
