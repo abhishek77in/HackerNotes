da0d4d8c561bb109515463e85eb65b986191e10c
abhishek srivastava <abhishek77in@gmail.com>
2013-04-26 00:49:56 +0530

    Get videos only from whitelisted sites

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,13 @@
 class Video
   include Plug
+  HOSTNAME_WHITELIST = %w(www.youtube.com youtube.com www.vimeo.com vimeo.com)
+  validate :whitelisted?, on: :create
+
+  private
+  def whitelisted?
+    hostname = URI.parse(self.url).hostname
+    unless HOSTNAME_WHITELIST.include?(hostname)
+      errors.add(:whitelist, "resource should belong to #{HOSTNAME_WHITELIST.join(', ')}")
+    end
+  end
 end
