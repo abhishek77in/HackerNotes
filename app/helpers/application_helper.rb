7ef98b59a32df0156f4288ad604427195224cad1
abhishek srivastava <abhishek77in@gmail.com>
2013-04-30 00:11:00 +0530

    WIP handle errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,23 +5,4 @@ module ApplicationHelper
     anchor = anchor || resources.first.model_name.downcase.pluralize
     { anchor: "##{anchor}", title: title, css_class: css_class }
   end
-
-  def error_message_for(object, attribute)
-    return if object.nil?
-    if (errors = object.errors[attribute]).present?
-      %Q[<div class="inlineValidationMessage">#{errors.join(', ')}</div>].html_safe
-    end
-  end
-
-  def global_validation_message
-    if flash[:alert].present?
-      %Q[<div class="globalValidationMessage">#{flash[:alert]}</div>].html_safe
-    end
-  end
-
-  def global_success_message
-    if flash[:notice].present?
-      %Q[<div class="globalSuccessMessage">#{flash[:notice]}</div>].html_safe
-    end
-  end
 end
