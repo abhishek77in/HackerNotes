3f87d47365f99d787eb7c7d107b19ad91a2b0411
abhishek srivastava <abhishek77in@gmail.com>
2013-05-20 23:01:59 +0530

    Show repositories of loggedin users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,15 @@ class HomeController < ApplicationController
   def homepage
     @repos = Repository.desc(:created_at).limit(10)
     @popular_repos = Repository.desc(:watchers_count).limit(15)
+
+    @user_repo_names = user_repo_names
+  end
+
+  private
+  def user_repo_names
+    return [] unless current_user
+    repos = Rails.cache.fetch(current_user.nickname, :expires_in => 1.weeks) do
+      Github.new(basic_auth: "#{ENV['GITHUB_LOGIN']}:#{ENV['GITHUB_PASSWORD']}").repos.list(user: current_user.nickname).map(&:name)
+    end
   end
 end
