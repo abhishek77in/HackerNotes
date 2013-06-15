544773fed19fec6cfcc9018c030ff95440c52da7
abhishek srivastava <abhishek77in@gmail.com>
2013-06-15 15:16:19 +0530

    Whitelist twitter.com for tweeters

--- app/models/tweeter.rb
+++ app/models/tweeter.rb
@@ -1,3 +1,13 @@
 class Tweeter
   include Plug
+  HOSTNAME_WHITELIST = %w(www.twitter.com twitter.com)
+  validate :whitelisted?, on: :create
+
+  private
+  def whitelisted?
+    hostname = URI.parse(self.url).hostname
+    unless HOSTNAME_WHITELIST.include?(hostname)
+      errors.add(:whitelist, "Tweeter should belong to twitter.com")
+    end
+  end
 end
