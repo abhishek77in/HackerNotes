9ab55c1207a403a400eea6adb6c84c148fa1a25e
abhishek srivastava <abhishek77in@gmail.com>
2013-06-16 00:40:05 +0530

    Allow only power user to remove resources

--- app/controllers/remove_controller.rb
+++ app/controllers/remove_controller.rb
@@ -3,6 +3,7 @@ class RemoveController < ApplicationController
   def remove
     return unless current_user.present?
     repo = GithubService.new.fetch(params[:owner], params[:name])
+    return unless current_user.power_user?(repo.owner)
     resource_type = params[:resource_type].downcase.pluralize
     repo.send(resource_type).where(id: params[:id]).first.delete
     render :text => 'removed'
--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -30,8 +30,12 @@ module RepositoriesHelper
     link_to(image_tag("vote_#{direction}.png"), '#', class: "vote vote#{direction.capitalize}", 'data-id' => resource.id, 'data-resource_type' => resource.model_name)
   end
 
-  def link_to_remove(resource)
-    link_to('remove', '#', class: "remove", 'data-id' => resource.id, 'data-resource_type' => resource.model_name)
+  def link_to_remove_if_power_user(resource, repo)
+    link_to('remove',
+            '#',
+            class: "remove",
+            'data-id' => resource.id,
+            'data-resource_type' => resource.model_name) if current_user.power_user?(repo.owner)
   end
 
   def description(repo)
