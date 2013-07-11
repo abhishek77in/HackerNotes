1495b7040def14789ba5726caa5e4cfbfc9bc609
Abhishek Srivastava <abhishek77in@gmail.com>
2013-07-11 18:32:06 +0530

    Fix - Issue with voting/removing subscription_sevices records

--- app/controllers/remove_controller.rb
+++ app/controllers/remove_controller.rb
@@ -4,7 +4,7 @@ class RemoveController < ApplicationController
     return unless current_user.present?
     repo = GithubService.new.fetch(params[:owner], params[:name])
     return unless current_user.power_user?(repo.owner)
-    resource_type = params[:resource_type].downcase.pluralize
+    resource_type = params[:resource_type].underscore.pluralize
     repo.send(resource_type).where(id: params[:id]).first.delete
     render :text => 'removed'
   end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,7 +3,7 @@ class VotesController < RepositoriesBaseController
   def vote
     return unless current_user.present?
     @repo = GithubService.new.fetch(params[:owner], params[:name])
-    resource_type = params[:resource_type].downcase.pluralize
+    resource_type = params[:resource_type].underscore.pluralize
     resource = @repo.send(resource_type).where(id: params[:id]).first
     resource.assign_vote(params[:direction], current_user)
     render :json => resource.votes_count
