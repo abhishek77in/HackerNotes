9b832851766d9af45da69d7a398c027c2ae621b7
Abhishek Srivastava <abhishek77in@gmail.com>
2013-07-13 20:57:01 +0530

    Add styling to suggestions

--- app/assets/javascripts/repositories.js
+++ app/assets/javascripts/repositories.js
@@ -22,13 +22,13 @@ function slider () {
 suggestions = {
   'blogs' : "Add any blog post you find relevent to this project.",
   'subscription_services' : "Add links to those websites which offer you to subscribe, this could also be a youtube channel as well.",
-  'tips' : "Tips n Tricks could be Coderwall protips which you find interesting or a even a blog post.",
-  'slides' : "You can add slides from speakerdeck.com or slideshare.net for now if you think we are missing something let us know.",
+  'tips' : "Tips 'n' Tricks could be Coderwall Protips which you find interesting or a even a blog post.",
+  'slides' : "You can add slides from speakerdeck.com or slideshare.net.",
   'qnas' : "Add good to read questions/answers from stackoverflow.com or quora.com",
   'screencasts' : "If you find a screencast you can link it here.",
   'videos' : "You can add videos from youtube.com or vimeo.com",
-  'tweeters' : "You can add people who tweet about this project, you can add them by adding link to their user profile.",
-  'noteworthies' : "This could be official website for the project, or websites related to the project"
+  'tweeters' : "Add people who tweet about this project, you can add them by adding link to their user profile page.",
+  'noteworthies' : "This could be official website for the project, or websites related to the project."
 }
 
 function suggest() {
