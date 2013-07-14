b3b5e13f05782a96240986c311a7098be685a170
Abhishek Srivastava <abhishek77in@gmail.com>
2013-07-15 00:21:08 +0530

    Refactor voting module

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,27 +1,45 @@
 module Vote
+  def assign_vote(direction, user)
+    username = user.nickname
+
+    if first_vote?(username)
+      cast_first_vote(direction, username)
+    else
+      vote_up(username) if direction == 'up'
+      vote_down(username) if direction == 'down'
+    end
+
+    self.save
+  end
+
+  def first_vote?(username)
+    self.votes[username].blank?
+  end
+
+  def cast_first_vote(direction, username)
+    if direction == 'up'
+      self.votes[username] = 1
+    elsif direction == 'down'
+      self.votes[username] = -1
+    end
+  end
+
+  def vote_up(username)
+    return if self.votes[username] == 1
+    self.votes[username] = self.votes[username] + 1
+  end
+
+  def vote_down(username)
+    return if self.votes[username] == -1
+    self.votes[username] = self.votes[username] - 1
+  end
 
   def votes_count
     self.votes.values.sum
   end
 
-  def assign_vote(direction, user)
-    if self.votes[user.nickname].present?
-      if direction == 'up'
-        self.votes[user.nickname] = (self.votes[user.nickname] + 1) unless self.votes[user.nickname] == 1
-      elsif direction == 'down'
-        self.votes[user.nickname] = (self.votes[user.nickname] - 1) unless self.votes[user.nickname] == -1
-      end
-    else
-      if direction == 'up'
-        self.votes[user.nickname] = 1
-      elsif direction == 'down'
-        self.votes[user.nickname] = -1
-      end
-    end
-    self.save
-  end
-
   def update_votes_count
     self.votes_counter = self.votes_count if self.votes_changed?
   end
+
 end
