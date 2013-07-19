0a0a50e67d33281e7011cbb7fd95c561973047e0
Abhishek Srivastava <abhishek77in@gmail.com>
2013-07-19 17:50:54 +0530

    Disable user profile for now

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Hackernotes::Application.routes.draw do
   get  ':owner/*name/*resource/*id', :to => 'notes#resource_iframe', :as => 'resource_iframe'
   get  ':owner/*name/*resource', :to => 'repositories#show_resources', :as => 'show_resources'
   get  ':owner/:name', :to => 'repositories#show', :as => 'show_repo'
-  get  ':username', :to => 'users#show', :as => 'user_profile'
+#  get  ':username', :to => 'users#show', :as => 'user_profile'
 end
