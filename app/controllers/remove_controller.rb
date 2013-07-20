a198ed52a625733fd076849579c48577e698060b
Abhishek Srivastava <abhishek77in@gmail.com>
2013-07-20 18:59:18 +0530

    Fix: Fix voting/removing on resources

--- app/controllers/remove_controller.rb
+++ app/controllers/remove_controller.rb
@@ -1,8 +1,8 @@
-class RemoveController < ApplicationController
+class RemoveController < RepositoriesBaseController
 
   def remove
     return unless current_user.present?
-    repo = GithubService.new.fetch(params[:owner], params[:name])
+    repo = find_repo(params)
     return unless current_user.power_user?(repo.owner)
     resource_type = params[:resource_type].underscore.pluralize
     repo.send(resource_type).where(id: params[:id]).first.delete
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,10 +2,10 @@ class VotesController < RepositoriesBaseController
 
   def vote
     return unless current_user.present?
-    @repo = GithubService.new.fetch(params[:owner], params[:name])
+    repo = find_repo(params)
 
     resource_type = params[:resource_type].underscore.pluralize
-    resource = @repo.send(resource_type).where(id: params[:id]).first
+    resource = repo.send(resource_type).where(id: params[:id]).first
 
     current_user.reward_karma(:vote, resource) if resource.first_vote?(current_user.nickname)
     resource.assign_vote(params[:direction], current_user)
