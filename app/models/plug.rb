9f8baa1bc1b415dfba785d706e7061e49098f1c8
Abhishek Srivastava <abhishek77in@gmail.com>
2013-07-28 08:03:08 +0530

    Fix: Do not require scheme of URL

--- app/models/plug.rb
+++ app/models/plug.rb
@@ -26,7 +26,7 @@ module Plug
     validates_presence_of :user, message: 'Login is required to add resources'
     # validates :title, presence: true, on: :create
     # validates :description, presence: true, on: :create
-    validates :url, presence: true, uniqueness: true, format: URI::regexp(%w(http https))
+    validates :url, presence: true, uniqueness: true
 
     default_scope desc(:votes_counter).where(:votes_counter.gte => -3).order_by(:created_at => :desc)
 
