1e062e18e912f1566f2f19027ee8b24dfc5bc88d
Abhishek Srivastava <abhishek77in@gmail.com>
2013-08-06 13:15:00 +0530

    Remove user repositories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,7 @@ class HomeController < ApplicationController
     @repos = Repository.desc(:created_at).limit(10)
     @popular_repos = Repository.desc(:watchers_count).limit(15)
 
-    @user_repo_names = user_repo_names
     @karma_leaders = User.desc(:total_karma).limit(5)
   end
 
-  private
-  def user_repo_names
-    return [] unless current_user
-    repos = Rails.cache.fetch(current_user.nickname, :expires_in => 1.hour) do
-      Github.new(basic_auth: "#{ENV['GITHUB_LOGIN']}:#{ENV['GITHUB_PASSWORD']}").repos.list(user: current_user.nickname).map(&:name)
-    end
-  end
 end
