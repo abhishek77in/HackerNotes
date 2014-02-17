cb323b22390686c30c06b0562e61a78917f7ee10
Abhishek Srivastava <abhishek77in@gmail.com>
2014-02-17 20:54:02 +0530

    Add task to update twitter image urls

--- app/service/tweeter_service.rb
+++ app/service/tweeter_service.rb
@@ -10,12 +10,12 @@ class TweeterService
   DESCRIPTION = ".bio"
   IMAGE = "img"
 
-  def client
+  def self.client
     Twitter::REST::Client.new do |config|
-      config.consumer_key        = TWITTER_CONSUMER_KEY
-      config.consumer_secret     = TWITTER_SECRET_KEY
-      config.access_token        = TWITTER_ACCESS_TOKEN
-      config.access_token_secret = TWITTER_ACCESS_TOKEN_SECRET
+      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TWITTER_SECRET_KEY']
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
     end
   end
 
