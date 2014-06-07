c8f5d04f52322b0ab43edd9cea403502bd961663
Abhishek Srivastava <abhishek77in@gmail.com>
2014-06-07 09:03:46 +0530

    Change user name

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-root = "/home/ubuntu/apps/HackerNotes/current"
+root = "/home/abhishek/apps/HackerNotes/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,10 +12,10 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/ubuntu/apps/HackerNotes/current
+APP_ROOT=/home/abhishek/apps/HackerNotes/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=ubuntu
+AS_USER=abhishek
 set -u
 
 OLD_PIN="$PID.oldbin"
